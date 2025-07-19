117cafafcaac8baf57dcf41bb298499e056616cc
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-07-19 23:05:02 +0300

    NotesListPage bileşeni içerisine useState ve useEffect ile not verisi çekme altyapısı kuruldu

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const NotesListPage = () => {
+
+  let [notes, setNotes] = useState()
+  useEffect(()=> {
+
+  },[] )
+
+  let getNotes = async() => {
+    fetch()
+  }
+
+
+
+
   return (
     <div>
       notes
