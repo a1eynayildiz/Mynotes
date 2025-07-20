598f6449aa2429b232eb86ef74ea9f9db20e547d
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-07-20 23:06:04 +0300

    CORS yapılandırması tamamlandı: corsheaders eklendi ve izinler tanımlandı

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -4,6 +4,7 @@ const NotesListPage = () => {
 
   let [notes, setNotes] = useState()
   useEffect(()=> {
+    getNotes()
 
   },[] )
 
@@ -11,10 +12,9 @@ const NotesListPage = () => {
     let response = await fetch('http://127.0.0.1:8000/api/notes/')
 
     let data = await response.json()
+    console.log('DATA:', data)
+    setNotes(data)
   }
-
-
-
 
   return (
     <div>
