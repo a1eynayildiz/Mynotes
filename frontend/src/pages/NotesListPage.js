d4451f0b252c38d6b1ea0e514560bdecfe13fe6f
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-07-20 23:31:54 +0300

    Frontend'i Django API'ye bağladım ve notları ekranda göstermeye başladım

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react'
 
 const NotesListPage = () => {
 
-  let [notes, setNotes] = useState()
-  useEffect(()=> {
-    getNotes()
+  let [notes, setNotes] = useState([])
 
-  },[] )
+  useEffect(() => {
+  getNotes()
+  }, [])
+
 
   let getNotes = async() => {
     let response = await fetch('http://127.0.0.1:8000/api/notes/')
@@ -18,7 +19,12 @@ const NotesListPage = () => {
 
   return (
     <div>
-      notes
+      <div className='notes-list'>
+        {notes.map((note, index)=>(
+          <h3>{note.body}</h3>
+        ))}
+
+      </div>
     </div>
   )
 }
