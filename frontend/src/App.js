e85ef6a613c61ea7dc14ce2b7eef9a82a317b52f
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-07-21 20:41:42 +0300

    Fix router structure: wrap Route inside Routes as required by React Router v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import {
   BrowserRouter as Router,
+  Routes,
   Route
 } from "react-router-dom";
 
-
-
-
 import './App.css';
 import Header from './components/Header';
-import NotesListPage from './pages/NotesListPage'
+import NotesListPage from './pages/NotesListPage';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Header/>
-      <Route path="/" exact component={NotesListPage}/>
-      
-    </div>
+      <div className="App">
+        <Header />
+        <Routes> {/* Route'ları artık Routes içine alıyoruz */}
+          <Route path="/" element={<NotesListPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
