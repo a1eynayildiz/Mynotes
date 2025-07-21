32321f182851dc0a43b8fe2c139c921a94a13080
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-07-21 15:53:40 +0300

    Add proxy setting and connect frontend with backend

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-
+import ListItem from '../components/ListItem'
 const NotesListPage = () => {
 
   let [notes, setNotes] = useState([])
@@ -10,7 +10,7 @@ const NotesListPage = () => {
 
 
   let getNotes = async() => {
-    let response = await fetch('http://127.0.0.1:8000/api/notes/')
+    let response = await fetch('/api/notes/')
 
     let data = await response.json()
     console.log('DATA:', data)
@@ -21,7 +21,7 @@ const NotesListPage = () => {
     <div>
       <div className='notes-list'>
         {notes.map((note, index)=>(
-          <h3 key={index}>{note.body}</h3>
+          <ListItem key={index} note={note}/>
         ))}
 
       </div>
