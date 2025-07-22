00e195340cd6526a2907255fd88010716c5f2504
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-07-22 23:30:37 +0300

    Add NotePage component to fetch and display single note by ID using useParams and useEffect

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 
 const NotePage = () => {
+  const { id } = useParams()
+  const [note, setNote] = useState(null) 
+
+  useEffect(()=> {
+    
+  }
+  )
+
+  const getNote = async () => {
+    let response = await fetch(`/api/notes/${id}/`)  // proxy sayesinde http://127.0.0.1:8000 yazmama artık gerek yok
+    let data = await response.json()
+    setNote(data)
+  }
+
   return (
     <div>
-      <h1>Single Note</h1>
+        <h1>Single Note {id}</h1>
     </div>
   )
 }
